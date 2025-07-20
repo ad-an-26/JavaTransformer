9b9ff35f6472c67dce8d8dc45592ec92b38d5561
ad-an-26 <adeeba.anjum3@gmail.com>
2025-07-20 20:40:50 +0530

    basic ui

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
 import streamlit as st
+from transformer import transform_java_code
+
+st.set_page_config(page_title="Java Modernizer: Java 8 ➜ Java 21", layout="wide")
+st.title("Java Transformer")
+st.write("Paste your Java 8 code below and get the modernized Java 21 version using GenAI!")
+
+java8_code = st.text_area("Java 8 Code", height=300, placeholder="Paste your java 8 code here")
+
+if st.button("Transform to Java 21!"):
+  if not java8_code.strip():
+    st.warning("Please enter Java 8 code to transform")
+  else:
+    with st.spinner("Transforming using GenAI..."):
+      java21_code = transform_java_code(java8_code)
+      st.success("Done! Here's your modernized java 21 code: ")
+      st.code(java21_code, language='java')
+    
